35eba5c8e5bc42f7ff83aa8e46b32d229556f954
wodms112-ai <wodms112@gmail.com>
2025-09-09 23:49:06 +0900

    250909자바스트립트

--- test/js/main.js
+++ test/js/main.js
@@ -6,9 +6,11 @@ $(document).ready(function(){
     (없는 애는 가만히 있고 있는애만 지움
     누가on클래스를 가지고 있는지 모르니까*/
     $('.tour .list ul li').on('mouseenter', function(){
-        //console.log('오버했다')
+        //li이에 오버했을때의 명령이다~
         $('.tour .list ul li').removeClass('on')
+        //li에 오버했을때 일단 on들어간거 다뺴
         $(this).addClass('on')
+        //오버한 나한테만 적용해줘
     })
     $('footer .right_area .family_site button.family_open').on('click', function(){
         console.log('클릭함')
