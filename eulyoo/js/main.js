a3ada26e07ee87cc9809676f0c97b2b981c8ddd7
wodms112-ai <wodms112@gmail.com>
2025-09-25 18:04:30 +0900

    .

--- eulyoo/js/main.js
+++ eulyoo/js/main.js
@@ -2,14 +2,12 @@ $(document).ready(function(){
     const visual_swiper = new Swiper('.visual .swiper', { /* 팝업을 감싼는 요소의 class명 */
 
         autoplay: {  /* 팝업 자동 실행 */
-        	delay: 8000,
+        	delay: 5000,
         	disableOnInteraction: true,
         },
 
         effect: "fade", /* fade 효과 */
-
         loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
-
         pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
             el: '.visual .paging', /* 해당 요소의 class명 */
             clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
@@ -27,15 +25,17 @@ $(document).ready(function(){
 
     $('.visual .ctrl_btn .btn_stop').on('click' , function(){
         visual_swiper.autoplay.stop();  /* 일시정지 기능 */
-        // $(this).hide() //hide=숨기기기능 , this=나 숨길께
-        $('.visual .ctrl_btn .btn_play').show()
+        $(this).hide() //hide=숨기기기능 , this=나 숨길께
+        $('.visual .ctrl_btn .btn_play').show() 
+        //정지버튼
+    })//종료
 
-    $('.visual .ctrl_btn .btn_play').on('click' , function (){
+    $('.visual .ctrl_btn .btn_play').on('click' , function(){
         visual_swiper.autoplay.start();    /* 재생 기능 */
-        // $(this).hide() //hide=숨기기기능 , this=나 숨길께
+        $(this).hide() //hide=숨기기기능 , this=나 숨길께
         $('.visual .ctrl_btn .btn_stop').show()
-    })
-    })// 버튼 종료
+
+    })// 종료
 
 
 
@@ -66,8 +66,24 @@ $(document).ready(function(){
     //브라우저가 스크롤 될 때 마다 1번씩 실행
     $(window).scroll(function(){
         scroll_chk()
-    })
+    }) //플레이버튼 종료
 
+
+    //book swiper
+    const book_swiper = new Swiper('.book .swiper', { /* 팝업을 감싼는 요소의 class명 */
+        slidesPerView: 2, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+        spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+        breakpoints: {
+           
+            769: {    /* 640px 이상일때 적용 */
+                slidesPerView: 'auto',    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+                spaceBetween: 24,
+            },
+        },
+        navigation: {  /* 이전, 다음 버튼 */
+        nextEl: '.book .ctrl_btn .btn_next',  /* 다음 버튼의 클래스명 */
+        prevEl: '.book .ctrl_btn .btn_prev',  
+    },
+    });//swiper 종료
 
 }) //맨끝
-
