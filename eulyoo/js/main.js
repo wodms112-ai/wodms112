5afefed59f5f05b3efdc51fff9c4035fee7e6233
wodms112-ai <wodms112@gmail.com>
2025-09-29 00:54:30 +0900

    .

--- eulyoo/js/main.js
+++ eulyoo/js/main.js
@@ -76,7 +76,7 @@ $(document).ready(function(){
         breakpoints: {
            
             769: {    /* 640px 이상일때 적용 */
-                slidesPerView: 1,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+                slidesPerView: 'auto',    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
                 spaceBetween: 24,
             },
         },
