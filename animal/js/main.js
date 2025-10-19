55153e952c98fe9614e133240c2e0d4ef2689596
wodms112-ai <wodms112@gmail.com>
2025-10-20 00:57:30 +0900

    .

--- animal/js/main.js
+++ animal/js/main.js
@@ -4,22 +4,22 @@ $(document).ready(function(){
 //**************************** 시작 : 현재 (pc)인지 ,(모바일)인지 체크 => 메뉴상태보고************************
     
     let mobile_size = 1024 //브라우저 너비가 1024px 이하이면 모바일
-    let window_w // 지금 브라우저의 실제 너비값(px) 을 담는 변수.
-    let device_status //지금 기기가 pc인지 mobile인지 상태를 저장할 변수
+    let window_w // 브라우저 넓이가 --이면 윈도우
+    let device_status //위 규칙을 담는 변수바구니
 
     function device_chk(){  // 함수를 선언 한다 (pc인지 mobile인지 구분해주는 검사기)
-        window_w = $(window).width() //현재 브라우저 창의 가로 길이(px 단위)가
-        if(window_w > mobile_size){ // 브라우저 넓이가 1024보다 클때
-            device_status = 'pc'    //=pc버전이다
+        window_w = $(window).width() // window_w 는 = 윈도우의 넓이다
+        if(window_w > mobile_size){ // 윈도우넓이 > 모바일넓이 이면
+            device_status = 'pc'    //변수바구니 => pc넣어
         }else{                      //아니면
-            device_status = 'mobile'//=모바일 버전이다
+            device_status = 'mobile'//=모바일 버전넣어
         }
         console.log(device_status)  //
     }
 
-    device_chk() //페이지가 처음 열릴 때 한 번 실행해서 초기 상태가 PC인지 모바일인지 확인해서
-    $(window).resize(function(){ // 윈도우 사이즈가 리사이즈 될때마다 실행
-        device_chk() 
+    device_chk() //페이지가 처음 열릴 때 한 번 사이즈 체크해서
+    $(window).resize(function(){ // 윈도우가 리사이즈 될때마다 실행해 
+        device_chk() //사이즈변수바구니에
     })
 
 
@@ -29,7 +29,7 @@ $(document).ready(function(){
 
 
 //**************************** visual swiper 시작 ****************************
-    let visual_time = 5000 /*변수 만들기-> 하나만 바뀌면 다 바뀌게 */
+    let visual_time = 5000 
     const visual_swiper = new Swiper('.visual .swiper', { /* 팝업을 감싼는 요소의 class명 */
 
         autoplay: {  /* 팝업 자동 실행 */
@@ -59,14 +59,16 @@ $(document).ready(function(){
         })
 
 
-    // 전체 슬라이드 개수 (loop 상태에서도 실제 슬라이드 개수만)
+    // 전체 슬라이드 개수 (loop 상태에서도 실제 슬라이드 개수만) /*? */
     const totalSlides = $('.visual .swiper .swiper-slide').not('.swiper-slide-duplicate').length;
-        $('.visual .paging .total').text(totalSlides); // 총 개수 표시
+                             // 이 슬라이드 하나하나를 찾아라!/“복사본은 빼고 진짜 슬라이드만 세라!
+        $('.visual .paging .total').text(totalSlides); 
+                           // 이 요소(예: “/5” )에 슬라이드수를 글자로 표시해줘!
 
     // 현재 슬라이드 번호 표시 함수
-    function updateCurrent() {
-        let realIndex = visual_swiper.realIndex + 1; // 실제 인덱스 (0부터 시작하므로 +1)
-        $('.visual .paging .current').text(realIndex);
+    function updateCurrent() { //처음 로드시 실행해
+        let realIndex = visual_swiper.realIndex + 1; // realIndex는 =실제 인덱스visual_swiper.realIndex +1이야 (0부터 시작하므로 +1)
+        $('.visual .paging .current').text(realIndex);//realIndex의 숫자를 글짜로 써줘
         //슬라이드가 교체되면 제일 먼저 넓이를0으로 초기화
         $('.visual .ctrl_btn .bar span').stop() //에니메이션종료
         $('.visual .ctrl_btn .bar span').width(0)
@@ -100,7 +102,8 @@ $(document).ready(function(){
  */ 
 
 
-$('header .gnb .gnb_wrap ul.depth1 > li').on('mouseenter focusin' , function(){ //? 마우스로 메뉴 위에 올렸을 때 작동시켜
+$('header .gnb .gnb_wrap ul.depth1 > li').on('mouseenter focusin' , function(){ 
+    //? 마우스로 메뉴 위에 올렸을 때 작동시켜
                                                       //focusin =키보드접근성
     if( device_status == 'pc'){  //pc일때만 이걸 동작해
         $('header').addClass('menu_pc') //헤더에 '메뉴 열림' 상태 표시
